37d7b513405d5679b1668995ec9d11564ef8aba5
sniper-ninja <rahilardejaa@gmail.com>
2020-05-07 06:07:36 +0530

    before final

--- js/main.js
+++ js/main.js
@@ -115,3 +115,21 @@
 	});
 
 })(jQuery);
+
+// our project
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"goDown",
+        navigationText:["",""],
+        autoPlay:false
+    });
+});
